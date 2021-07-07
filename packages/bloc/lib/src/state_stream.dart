fe507606f43f23a69a0f02d8f3acb065ea56c05a
Plague Fox <plugfox@gmail.com>
2021-07-08 01:13:12 +0300

    [#11] add toStream() upcast

--- packages/bloc/lib/src/state_stream.dart
+++ packages/bloc/lib/src/state_stream.dart
@@ -11,6 +11,10 @@ class StateStream<State extends Object?> extends StreamView<State> {
       : assert(stream.isBroadcast, 'State stream must be broadcast'),
         super(stream);
 
+  /// This stream is always broadcast
+  @override
+  bool get isBroadcast => true;
+
   /// This transformer is a shorthand for Stream.where followed by Stream.cast.
   ///
   /// [State]'s that do not match [T] are filtered out,
@@ -20,17 +24,27 @@ class StateStream<State extends Object?> extends StreamView<State> {
 
   /// Filter with whereState<T>() and after that
   /// skips [State]'s if they are equal to the previous data event.
+  ///
+  /// [State]'s that do not match [T] are filtered out,
+  ///  the resulting Stream will be of Type [T].
   Stream<T> whereUnique<T extends State>() => whereState<T>().distinct();
 
+  /// This transformer leaves only the necessary states with downcast to [T]
   ///
-  ///
-  ///
+  /// [State]'s that do not match [T] are filtered out,
+  ///  the resulting Stream will be of Type [T].
   Stream<T> whereStates<T extends State>(bool Function(State state) filter) =>
       where(filter).where((state) => state is T).cast<T>();
 
+  /// Filter with whereStates<T>() and after that
+  /// skips [State]'s if they are equal to the previous data event.
+  /// Downcast states to [T]
   ///
-  ///
-  ///
+  /// [State]'s that do not match [T] are filtered out,
+  ///  the resulting Stream will be of Type [T].
   Stream<T> whereUniques<T extends State>(bool Function(State state) filter) =>
       whereStates<T>(filter).distinct();
+
+  /// Upcast this object to Stream<State>
+  Stream<State> toStream() => this;
 }
