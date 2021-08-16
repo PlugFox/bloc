be5e5277e1c7c7b0b5422ce38c47dcd9bad92f06
Plague Fox <plugfox@gmail.com>
2021-08-17 02:40:36 +0300

    [#23] export some classes

--- packages/scope/lib/scope.dart
+++ packages/scope/lib/scope.dart
@@ -2,4 +2,7 @@ library scope;
 
 export './src/exceptions.dart';
 export './src/extensions.dart';
+export './src/global_scope.dart';
+export './src/multi_scope.dart';
 export './src/scope.dart';
+export './src/zoned_scope.dart';
