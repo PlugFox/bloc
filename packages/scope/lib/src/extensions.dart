7affec55af9cd363e4090639ca0f123438950b44
Plague Fox <plugfox@gmail.com>
2021-08-18 11:48:18 +0300

    [#10] add tryRead, tryWatch

--- packages/scope/lib/src/extensions.dart
+++ packages/scope/lib/src/extensions.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/widgets.dart' show BuildContext;
 
+import 'exceptions.dart';
 import 'scope.dart' show Scope;
 
 /// TODO: doc
 extension BuildContextScopeX on BuildContext {
   /// TODO: doc
-  T read<T extends Object>() => Scope.of<T>(this, listen: false);
+  T read<T extends Object>({final T Function()? orElse}) {
+    final value = Scope.maybeOf<T>(this, listen: false);
+    if (value != null) {
+      return value;
+    } else if (orElse != null) {
+      return orElse();
+    }
+    throw ScopeNotFoundException(T);
+  }
 
   /// TODO: doc
-  T watch<T extends Object>() => Scope.of<T>(this, listen: true);
+  T watch<T extends Object>({final T Function()? orElse}) {
+    final value = Scope.maybeOf<T>(this, listen: true);
+    if (value != null) {
+      return value;
+    } else if (orElse != null) {
+      return orElse();
+    }
+    throw ScopeNotFoundException(T);
+  }
+
+  /// TODO: doc
+  T? tryRead<T extends Object>() => Scope.maybeOf<T>(this, listen: false);
+
+  /// TODO: doc
+  T? tryWatch<T extends Object>() => Scope.maybeOf<T>(this, listen: true);
 }
